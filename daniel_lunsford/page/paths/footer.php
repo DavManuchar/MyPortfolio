43388147295085850a27f24944486e2d7e1cc06c
DavManuchar <davitman2007@mail.ru>
2025-03-07 22:46:18 +0300

    Add files via upload

---
+++ daniel_lunsford/page/paths/footer.php
@@ -0,0 +1,38 @@
+<footer>
+  <div class="footer_up">
+    <div class="text_box">
+      <div class="text_box_div">
+        <span class="s1">For media inquiries,</span>
+        <span class="s1">please contact agent Donna Clark</span>
+        <span class="s2">123-456-7890 | info@my-domain.com
+          <span>500 Terry Francine Street,  San Francisco</span>
+        </span>
+      </div>
+    </div>
+    <div class="text_box">
+      <div class="text_box_div2">
+        <span class="s1">Sign up for news and updates </span>
+        <span class="s1">from Daniel Lunsford</span>
+        <div class="input_div">
+          <input type="email" class="input" placeholder="Enter you email here*">
+        </div>
+        <div class="input_div">
+          <button>
+            Subscribe
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="text_box">
+      <div class="text_box_div1">
+        <span class="s1">Find more information about</span>
+        <span class="s1">our online store & policies below</span>
+        <span class="s2">FAQ | Shipping & Returns 
+          <span>Store Policy | Payment Methods</span>
+        </span>
+      </div>
+    </div>
+  </div>
+</footer>
+</body>
+</html>
